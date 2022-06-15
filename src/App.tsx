da32971708df1a88b6098e0fbf88637a2c5022d5
Tyler Sustare <10850753+TylerSustare@users.noreply.github.com>
2022-06-15 11:28:40 -0700

    copy game link to clipboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { tap } from "rxjs/operators";
+import { webSocket } from "rxjs/webSocket";
+import { useClipboard } from "./useClipboard";
+import "wired-elements";
+import "./App.css";
+
+const wsURL = "wss://cbwfvjy7j8.execute-api.us-west-2.amazonaws.com/Prod";
+const wsConfig = {
+  url: wsURL,
+  openObserver: {
+    next: () => {
+      console.log("connected");
+      const gameId = window.location.pathname?.slice(1);
+      console.log("gameId", gameId);
+      if (!gameId) {
+        return ws.next({
+          action: "new",
+          userId: "abc123",
+        });
+      }
+      console.log("attempting to connect to " + gameId);
+      ws.next({
+        action: "join",
+        userId: "abc123",
+        gameId: gameId,
+      });
+    },
+  },
+  closingObserver: {
+    next: () => {
+      // if (event.wasClean)
+      console.log("closingObserver");
+      ws.complete();
+    },
+  },
+};
+interface Message {
+  action: string;
+  userId: string;
+  gameId?: string;
+}
+const ws = webSocket<Message>(wsConfig);
 
 function App() {
+  const [message, setMessage] = React.useState({} as Message);
+  const [gameId, setGameId] = React.useState("");
+  const [isCopied, copy] = useClipboard({ successDuration: 6000 });
+  ws.pipe(
+    tap((data) => {
+      console.log(data);
+      setMessage(data);
+    })
+  ).subscribe();
+
+  // check for gameId from path
+  if (!gameId) {
+    const path = window.location.pathname?.slice(1);
+    if (path) setGameId(path);
+  }
+
+  // push gameId to path if not already there
+  if (!gameId && message.gameId) {
+    window.history.pushState(null, "", `/${message.gameId}`);
+  }
+
+  function copyToClipboard() {
+    copy(window.location.href);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <wired-card elevation="3">
+        <p>Game ID: {message.gameId}</p>
+        <wired-button onClick={copyToClipboard}>Copy Game Link</wired-button>
+        {isCopied && <p>Copied!</p>}
+      </wired-card>
     </div>
   );
 }
---
+++ src/useClipboard.tsx
@@ -0,0 +1,37 @@
+import { useState, useEffect } from "react";
+import copy from "copy-to-clipboard";
+
+interface IOptions {
+  /**
+   * Reset the status after a certain number of milliseconds. This is useful
+   * for showing a temporary success message.
+   */
+  successDuration?: number;
+}
+
+export function useClipboard(
+  options?: IOptions
+): [boolean, (text: string) => void] {
+  const [isCopied, setIsCopied] = useState(false);
+  const successDuration = options && options.successDuration;
+
+  useEffect(() => {
+    if (isCopied && successDuration) {
+      const id = setTimeout(() => {
+        setIsCopied(false);
+      }, successDuration);
+
+      return () => {
+        clearTimeout(id);
+      };
+    }
+  }, [isCopied, successDuration]);
+
+  return [
+    isCopied,
+    (text: string) => {
+      const didCopy = copy(text);
+      setIsCopied(didCopy);
+    },
+  ];
+}
